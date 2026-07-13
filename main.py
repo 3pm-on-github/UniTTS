13b6db78d15b27d7f1f754e1e7d88df4ee747b05
3pm-on-github <3pm@threepm.xyz>
2026-07-13 16:03:23 +0200

    fix links and fakenitro emojis in messages

--- main.py
+++ main.py
@@ -25,6 +25,18 @@ def read_data():
 def write_data(data):
     return open("data.json", "w").write(json.dumps(data))
 
+def filter(message, msg):
+    if message.startswith("https://"): return
+    for mention in msg.mentions:
+        name = mention.nick or mention.global_name
+        message = re.sub(rf"<@!?{mention.id}>", name, message)
+    message = re.sub(r"[\w+](https://\w+)", "", message)
+    message = re.sub(r"https://\w+", "", message)
+    message = re.sub(r"<t:\d+:\w+>", "", message)
+    message = re.sub(r"<:\w+:\d+>", "", message)
+    message = re.sub(r"<a:\w+:\d+>", "", message)
+    message = message.encode("ascii", "ignore").decode("ascii")
+
 def _play_next(error=None):
     if error:
         print(f"Playback error: {error}")
@@ -33,13 +45,7 @@ def _play_next(error=None):
         filename = f"{msg.id}.mp3"
         voice = read_data()["user_settings"][str(msg.author.id)]["voice"]
         message = msg.content[1:].strip() if msg.content.startswith("$") else msg.content.strip()
-        if message.startswith("https://"): return
-        for mention in msg.mentions:
-            name = mention.nick or mention.global_name
-            message = re.sub(rf"<@!?{mention.id}>", name, message)
-        message = re.sub(r"<t:\d+:\w+>", "", message)
-        message = re.sub(r"<:\w+:\d+>", "", message)
-        message = message.encode("ascii", "ignore").decode("ascii")
+        message = filter(message, msg)
         if not message: return
         generate_tts(message, voice, filename)
         def after(error):
@@ -134,13 +140,7 @@ async def on_message(msg):
             filename = f"{msg.id}.mp3"
             voice = data["user_settings"][str(msg.author.id)]["voice"]
             message = msg.content[1:].strip() if msg.content.startswith("$") else msg.content.strip()
-            if message.startswith("https://"): return
-            for mention in msg.mentions:
-                name = mention.nick or mention.global_name or mention.name
-                message = re.sub(rf"<@!?{mention.id}>", name, message)
-            message = re.sub(r"<t:\d+:\w+>", "", message)
-            message = re.sub(r"<:\w+:\d+>", "", message)
-            message = message.encode("ascii", "ignore").decode("ascii")
+            message = filter(message, msg)
             if not message or not any(c.isalpha() for c in message): return
             generate_tts(message, voice, filename)
             def after(error):

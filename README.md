a601896c2f7d8429d857f6ad2936f747e685f058
3pm-on-github <3pm@threepm.xyz>
2026-08-09 00:09:53 +0200

    added sam executables added readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# StarTTS
+General TTS bot with custom features
+
+## Features
+### Custom TTS Voices:
+- Google TTS (25 Languages)
+- SAM C64 (Custom Pitch, Speed, Mouth and Throat)
+- Microsoft SAM (Custom Pitch, Speed)
+### TTS if message contains prefix:
+Example:
+`$This will be played as TTS`
+`This won't be played as TTS`
+### Custom SFX:
+`fish` plays a "fish" SFX.
--- tts_gen.py
+++ tts_gen.py
@@ -1,4 +1,4 @@
-import subprocess, random, requests, os
+import subprocess, random, requests, os, platform
 from gtts import gTTS # type: ignore
 sfx_list = {
     "MI BOMBO": "mibombo.mp3",
@@ -28,36 +28,27 @@ def generate_tts(message, voice, filename):
         mouth = str(voice["mouth"])
         throat = str(voice["throat"])
         randomnum = random.randint(0, 999)
-        try:
-            if os.name == "nt":
-                subprocess.run([
-                    "sam.exe",
-                    "-wav", str(randomnum)+".wav",
-                    message,
-                    "-pitch", str(pitch),
-                    "-speed", str(speed),
-                    "-mouth", str(mouth),
-                    "-throat", str(throat),
-                ])
-            else:
-                subprocess.run([
-                    "./sam",
-                    "-wav", str(randomnum)+".wav",
-                    message,
-                    "-pitch", str(pitch),
-                    "-speed", str(speed),
-                    "-mouth", str(mouth),
-                    "-throat", str(throat),
-                ])
-                os.system(f"ffmpeg -i {str(randomnum)}.wav -af \"volume=0.5\" -b:a 320k {filename}")
-                os.system(f"rm {str(randomnum)}.wav")
-        except FileNotFoundError:
-            print("im evil.")
-            pitch = str(voice["pitch"])
-            speed = str(voice["speed"])
-            r = requests.get(f"https://samtts.com/api/demo/sapi4-tts?text={message}&voice=Sam&pitch={str(pitch)}&speed={str(speed)}")
-            open(str(randomnum)+".wav", "wb").write(r.content)
-            os.system(f"ffmpeg -i {str(randomnum)}.wav -af \"volume=1.0\" -b:a 320k {filename}")
+        if os.name == "nt":
+            subprocess.run([
+                "./assets/executables/sam.exe",
+                "-wav", str(randomnum)+".wav",
+                message,
+                "-pitch", str(pitch),
+                "-speed", str(speed),
+                "-mouth", str(mouth),
+                "-throat", str(throat),
+            ])
+        else:
+            subprocess.run([
+                "./assets/executables/sam" if platform.machine().lower() != "arm64" or platform.machine().lower() != "aarch64" else "./assets/executables/sam-arm",
+                "-wav", str(randomnum)+".wav",
+                message,
+                "-pitch", str(pitch),
+                "-speed", str(speed),
+                "-mouth", str(mouth),
+                "-throat", str(throat),
+            ])
+            os.system(f"ffmpeg -i {str(randomnum)}.wav -af \"volume=0.5\" -b:a 320k {filename}")
             os.system(f"rm {str(randomnum)}.wav")
     elif voice["type"] == "ms-sam":
         randomnum = random.randint(0, 999)

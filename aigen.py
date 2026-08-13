55e9ede117b927cc0b5308c2be99fa84f48cf526
3pm-on-github <3pm@threepm.xyz>
2026-08-13 22:18:19 +0200

    bugfix

--- aigen.py
+++ aigen.py
@@ -1,16 +1,18 @@
 import requests, json, random
 
-imageid = str(random.randint(1, 10000000))
-id = str(random.randint(1, 10000000))
+imageid = str(random.randint(1, 800000))
+id = str(random.randint(1, 800000))
 
 def generate(prompt: str, image: bool = False) -> str:
     if image:
         r = requests.post(f"https://chatbox.computer.com/api/image/prompt/{imageid}/create/", json={"prompt": prompt})
         img_r = requests.get(r.json()["image"])
+        print(r.text)
         open(r.json()["image"].split("/")[-1], "wb").write(img_r.content)
         return r.json()["image"].split("/")[-1]
     else:
         r = requests.post(f"https://chatbox.computer.com/api/questions/{id}/stream/", json={"question": "answer in english: "+prompt})
+        print(r.text)
         toreturn = ""
         for line in r.text.split("\n"):
             if line != "":

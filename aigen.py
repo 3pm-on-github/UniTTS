b639e20ec89b945eff7d990869dd1c3a3e00778e
3pm-on-github <3pm@threepm.xyz>
2026-08-13 22:09:58 +0200

    the dementia update

--- aigen.py
+++ aigen.py
@@ -1,13 +1,16 @@
-import requests, json
+import requests, json, random
+
+imageid = str(random.randint(1, 10000000))
+id = str(random.randint(1, 10000000))
 
 def generate(prompt: str, image: bool = False) -> str:
     if image:
-        r = requests.post("https://chatbox.computer.com/api/image/prompt/1/create/", json={"prompt": prompt})
+        r = requests.post(f"https://chatbox.computer.com/api/image/prompt/{imageid}/create/", json={"prompt": prompt})
         img_r = requests.get(r.json()["image"])
         open(r.json()["image"].split("/")[-1], "wb").write(img_r.content)
         return r.json()["image"].split("/")[-1]
     else:
-        r = requests.post("https://chatbox.computer.com/api/questions/1/stream/", json={"question": "answer in english: "+prompt})
+        r = requests.post(f"https://chatbox.computer.com/api/questions/{id}/stream/", json={"question": "answer in english: "+prompt})
         toreturn = ""
         for line in r.text.split("\n"):
             if line != "":
